b34483e8df4aa745c81d577c40ada4ad10c342f9
Malek Atwiz <malekatwiz@gmail.com>
2019-05-19 17:58:57 -0400

    Add project files.

---
+++ Chatter.UI/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.DependencyInjection;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace Chatter.UI.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static void RegisterIdentityServer(this IServiceCollection services)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = ProtocolTypes.OpenIdConnect;
+            })
+            .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddOpenIdConnect(ProtocolTypes.OpenIdConnect, options =>
+            {
+                options.Authority = "https://localhost:44331/";
+                options.RequireHttpsMetadata = false;
+
+                options.ClientId = "Chatter.UI";
+                options.ResponseType = TokenTypes.IdentityToken;
+                options.SaveTokens = true;
+            });
+        }
+    }
+}
---
+++ Chatter.UI/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Chatter.UI.Pages
+{
+    [Authorize]
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
